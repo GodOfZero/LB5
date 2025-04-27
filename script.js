1b7dc9fc64ae0c680f760a54748a798525631318
Oleksandr Matiashov <matiashovsasha@gmail.com>
2025-04-27 16:27:21 +0300

    1.0.1

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const cityInput = document.getElementById('cityInput');
     const fetchWeatherButton = document.getElementById('fetchWeather');
     const weatherDataDiv = document.getElementById('weatherData');
-    const apiKey = '667971076cc534cec10b7fa11dcb8f48'; // Замініть на свій реальний API ключ
+    const apiKey = '667971076cc534cec10b7fa11dcb8f48';
 
     fetchWeatherButton.addEventListener('click', () => {
-        const city = 'Kyiv';
-        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`; // Додано &units=metric для отримання температури в градусах Цельсія
+        const city = cityInput.value.trim();
+
+        if (city === '') {
+            weatherDataDiv.textContent = 'Будь ласка, введіть назву міста.';
+            return;
+        }
+
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
         fetch(apiUrl)
             .then(response => {
@@ -20,6 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 const windSpeed = data.wind.speed;
 
                 weatherDataDiv.innerHTML = `
+                    <h3>Погода в місті ${city}</h3>
                     <p>Температура: ${temperature}°C</p>
                     <p>Вологість: ${humidity}%</p>
                     <p>Швидкість вітру: ${windSpeed} м/с</p>
@@ -27,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
             })
             .catch(error => {
                 console.error('Помилка отримання даних про погоду:', error);
-                weatherDataDiv.textContent = 'Не вдалося отримати дані про погоду.';
+                weatherDataDiv.textContent = `Не вдалося отримати дані про погоду для міста "${city}". Перевірте назву міста та підключення до Інтернету.`;
             });
     });
 });
